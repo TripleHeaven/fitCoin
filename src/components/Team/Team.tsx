c7be555b65292ac1bd70750d23b4077824709835
TripleHeaven <heaven30.buroff@gmail.com>
2022-05-27 16:52:54 +0300

    feat time

--- src/components/Team/Team.tsx
+++ src/components/Team/Team.tsx
@@ -28,74 +28,96 @@ export const TeamWrapper = () => {
         }}
         alt="background"
       /> */}
-
-      <section className="flex flex-col items-center">
-        <Title className="text-gray-300" variant={TitleVariant.h1}>
-          team
-        </Title>
-        <Title variant={TitleVariant.h2}>about us</Title>
-        <Collapse
-          className="mt-[24px]"
-          toHeight={"max-h-[271px]"}
-          open={isOpenReadMore}
-          style={
-            !isOpenReadMore
-              ? {
-                  background: "-webkit-linear-gradient(90deg, #eee, #333)",
-                  WebkitBackgroundClip: "text",
-                  WebkitTextFillColor: "transparent",
-                }
-              : {}
-          }
-        >
-          <Paragraph>
-            We are a team of professional and passionate people and we are
-            specialized in e-commerce technologies. We have been around for more
-            than 15 years and have acquired skills and knowledge to build
-            architecture to support the web 3.0 movement.
-            <br />
-            <br /> We have been looking for a long-term monetization model that
-            does not jeopardise the user experience or privacy. Our objective is
-            to transform centralized platforms into sophisticated decentralised
-            organisational models that will encourage the development of
-            customized digital economies.
-            <br />
-            <br /> Our token is positioned as the first online commerce
-            cryptocurrency specifically designed for digital commerce and
-            delivers a new roadmap to social communications.
-            <br />
-            <br /> Our mission is to promote the creation of an open digital
-            services ecosystem through online commerce.
-            <br />
-            <br /> We have been a close observer of the growing momentum of
-            decentralized technologies networks and encourage the development of
-            digital economies. In such an ecosystem, economic incentives, other
-            than advertising, to make great products.
-          </Paragraph>
-        </Collapse>
-        {!isOpenReadMore && (
-          <div className="flex mt-[16px]">
-            <Button
-              variant={ButtonVariants.custom}
-              onClick={() => setOpenReadMore((prev) => !prev)}
-              className="mx-auto text-[24px] font-main font-[400] text-gray-200"
-            >
-              <div className="flex">
-                <span>Read more</span>
-                <img className="ml-[6px]" src="./icons/arrowDownRead.svg" />
-              </div>
-            </Button>
-          </div>
-        )}
+      <section className="relative w-full">
+        <img
+          src="./teams/linesSvg.svg"
+          className="hidden xl:block"
+          style={{ position: "absolute", top: "110px" }}
+        />
+        <img
+          src="./icons/xx.svg"
+          className="absolute sm:w-auto w-[12px] right-[0] sm:right-[100px]"
+        />
+        <section className="relative flex flex-col items-center max-w-[942px] mx-auto px-[24px] mt-[64px]">
+          <Title className="text-gray-300" variant={TitleVariant.h1}>
+            team
+          </Title>
+          <Title variant={TitleVariant.h2} className="mt-[16px]">
+            about us
+          </Title>
+          <Collapse
+            className="mt-[24px]"
+            toHeight={"max-h-[271px]"}
+            open={isOpenReadMore}
+            style={
+              !isOpenReadMore
+                ? {
+                    background: "-webkit-linear-gradient(90deg, #eee, #333)",
+                    WebkitBackgroundClip: "text",
+                    WebkitTextFillColor: "transparent",
+                  }
+                : {}
+            }
+          >
+            <Paragraph>
+              We are a team of professional and passionate people and we are
+              specialized in e-commerce technologies. We have been around for
+              more than 15 years and have acquired skills and knowledge to build
+              architecture to support the web 3.0 movement.
+              <br />
+              <br /> We have been looking for a long-term monetization model
+              that does not jeopardise the user experience or privacy. Our
+              objective is to transform centralized platforms into sophisticated
+              decentralised organisational models that will encourage the
+              development of customized digital economies.
+              <br />
+              <br /> Our token is positioned as the first online commerce
+              cryptocurrency specifically designed for digital commerce and
+              delivers a new roadmap to social communications.
+              <br />
+              <br /> Our mission is to promote the creation of an open digital
+              services ecosystem through online commerce.
+              <br />
+              <br /> We have been a close observer of the growing momentum of
+              decentralized technologies networks and encourage the development
+              of digital economies. In such an ecosystem, economic incentives,
+              other than advertising, to make great products.
+            </Paragraph>
+          </Collapse>
+          {!isOpenReadMore && (
+            <div className="flex mt-[16px]">
+              <Button
+                variant={ButtonVariants.custom}
+                onClick={() => setOpenReadMore((prev) => !prev)}
+                className="mx-auto text-[24px] font-main font-[400] text-gray-200"
+              >
+                <div className="flex">
+                  <span>Read more</span>
+                  <img className="ml-[6px]" src="./icons/arrowDownRead.svg" />
+                </div>
+              </Button>
+            </div>
+          )}
+        </section>
       </section>
-      <section className="items-center text-center w-full">
+      <img
+        src="./teams/greenDots.svg"
+        className="absolute sm:w-auto   right-[0px] w-[156px]    "
+      />
+
+      <section className="relative items-center text-center w-full mt-[120px]">
         <Title variant={TitleVariant.h2}>core team</Title>
         <section
           className={clsx(
-            "flex flex-wrap justify-center mx-auto mt-[64px]",
+            "flex flex-wrap justify-center mx-auto mt-[64px] sm:gap-[40px]",
             styles.coreTeamContainer
           )}
         >
+          <img
+            src="./teams/xxxGray.svg"
+            className="absolute left-[0px] bottom-[1100px] w-[12px] sm:w-auto sm:left-[50px] sm:bottom-[150px]"
+          />
+
           <TeamMember
             name="maxime gagne"
             role="Founder and CEO, Revolution Nutrition"
@@ -177,6 +199,10 @@ export const TeamWrapper = () => {
             }
           />
         </section>
+        <img
+          src="./teams/grayDots.svg"
+          className="absolute right-[0px] bottom-[-100px]"
+        />
       </section>
     </section>
   );
